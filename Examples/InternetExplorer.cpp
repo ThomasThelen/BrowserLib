3341eac37e12b3445d8deb9eaa584176653639c8
Thomas Thelen <tommythelen@gmail.com>
2015-07-30 15:18:49 -0700

    Create InternetExplorer.cpp

---
+++ Examples/InternetExplorer.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <string>
+#include <map>
+#include <shlobj.h>
+#include "BrowserLib.h"
+
+
+int main()
+{
+	IExplorer iexplorer;
+
+	if (exists("IExplorer")==1)
+	{
+		cout << "Internet Explorer is installed with version " <<iexplorer.GetVersion() << endl;
+		cout << "Its home page is " << iexplorer.GetHomePage() << endl;
+		cout << "Its exe path is set to" << iexplorer.GetInstallPath() << endl;
+		cout << "It's default serch engine url is " << iexplorer.GetSearchEngine() << endl;
+		if (iexplorer.GetPhishingStatus(80) == 1)
+		{
+			cout << "The phishing filter is on" << endl;
+		}
+}
+
+system("pause");
+return 0;
+}
